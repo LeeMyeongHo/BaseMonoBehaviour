344923221090e2b0ca4b37983e39e6ca9aac604e
LeeMyeongHo <42070276+LeeMyeongHo@users.noreply.github.com>
2019-07-27 23:19:29 +0900

    Add files via upload
    BaseMonoBehaviour, UpdateManager

---
+++ BaseMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseMonoBehaviour : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        UpdateManager.Instance.RegisterMonoBehaviour(this);
+    }
+
+    private void OnDisable()
+    {
+        UpdateManager.Instance.DeregisterMonoBehaviour(this);
+    }
+
+    public abstract void OnUpdate();
+}
---
+++ UpdateManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateManager : MonoBehaviour
+{
+    // 각 프로젝트에서 사용하는 싱글톤으로 바꿔주자.
+    private static UpdateManager instance = null;
+    public static UpdateManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject temp = new GameObject("UpdateManager");
+                instance = temp.AddComponent<UpdateManager>();
+            }
+            return instance;
+        }
+    }
+
+    private List<BaseMonoBehaviour> baseMonoBehaviours = new List<BaseMonoBehaviour>();
+
+    void Update()
+    {
+        for (int i = 0; i < instance.baseMonoBehaviours.Count; ++i)
+        {
+            instance.baseMonoBehaviours[i].OnUpdate();
+        }
+    }
+
+    public void RegisterMonoBehaviour(BaseMonoBehaviour obj)
+    {
+        if (instance.baseMonoBehaviours.Contains(obj) == false)
+        {
+            instance.baseMonoBehaviours.Add(obj);
+        }
+    }
+
+    public void DeregisterMonoBehaviour(BaseMonoBehaviour obj)
+    {
+        if (instance.baseMonoBehaviours.Contains(obj) == true)
+        {
+            instance.baseMonoBehaviours.Remove(obj);
+        }
+    }
+}
